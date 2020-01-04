4dd6e3d9b84f22136eafe4883003d2878a308306
khernawawi <khernawawi@gmail.com>
2020-01-04 20:06:01 +0800

    update css admin

--- test/navpanel.php
+++ test/navpanel.php
@@ -10,7 +10,7 @@ if(isset($_SESSION['email']))
   <head>
     <title>Tommorrowland Admin Panel</title>
     <!-- Main CSS-->
-    <link rel="stylesheet" type="text/css" href="main.css">
+     <link href="css/main.css" rel="stylesheet">
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
@@ -18,7 +18,7 @@ if(isset($_SESSION['email']))
   <body class="app sidebar-mini rtl">
    
    <!-- Navbar-->
-    <header class="app-header"><a class="app-header__logo" href="index.php">Appointment System</a>
+    <header class="app-header"><a class="app-header__logo" href="index.php">Admin Panel</a>
       <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
       </header>
     <!-- Sidebar menu-->
