2e91eb270bb2931d5f55fd934aac42cbfd71400e
Aiman Remy <aiman.dragonz@gmail.com>
2020-01-06 21:33:17 +0800

    minor fix
    add loading animation after register

--- process.php
+++ process.php
@@ -1,8 +1,25 @@
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+	 <link href="assets/css/font-awesome.css" rel="stylesheet" />
+	 <link href="assets/css/loading.css" rel="stylesheet" />
+</head>
 
 <body>
+
+	<!-- Animation part -->
+<div class="loader">   
+	<span class="box"></span>   
+	<span class="box"></span>  
+<div class="code"> 
+<img src="assets/img/no_background.png" width="120px">
+</div>    
+<span class="txt">Registering'...</span>
+</div>
+
+	<video id="videoBG" autoplay muted loop>
+		<source src="assets/vid/Tomorrowland_signin.mp4" type="video/mp4">
+		</video>
 
 <?php
 
@@ -25,9 +42,9 @@ $query = "SELECT * FROM user WHERE email='$email' ";
 $sql_result = mysqli_query($con , $query);
 if (mysqli_num_rows($sql_result)>0) //to return the query result in number of rows
 {
-	header("refresh:4.0; url=register.html");
- 	die("Email account is already exist. Please try to register again.");
-
+	header("refresh:4.0; url=register.html?error=email already doesn't exist");
+ 	//die("Email account is already exist. Please try to register again.");
+die();
 }
 
 
@@ -40,15 +57,15 @@ else
 
 	if($result)
 		{
-	 	echo "You have successfully registered in Tomorrowland!";
+	 	//echo "You have successfully registered in Tomorrowland!";
 		mysqli_close($con);
 
-		header("refresh:4.0; url=signin.php");
+		header("refresh:3.0; url=signin.php");
 		}
 	else
 	 	{
-	 	echo "Error in registration new user. Please try to register again."; 
-	 	header("refresh:4.0; url=register.html");
+	 	//echo "Error in registration new user. Please try to register again."; 
+	 	header("refresh:3.0; url=register.html");
 		}
 	}
 
@@ -56,5 +73,4 @@ else
 
 
 </body>
-
 </html>
