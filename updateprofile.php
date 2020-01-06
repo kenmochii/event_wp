0f03679a88d270e8f63020c4e5d71e9aa0ee342f
Skylate97 <aliftechno01@gmail.com>
2020-01-06 16:45:29 +0800

    added animation at user update

--- updateprofile.php
+++ updateprofile.php
@@ -11,10 +11,36 @@ $sql_result=mysqli_query($con,$sql) or die("Error in sql due to ".mysql_error())
 
 if($sql_result)
 
-	header ("refresh:1;url=userprofile.php?message=Data has been updated");
+	header ("refresh:3;url=userprofile.php?message=Data has been updated");
 
  else
  
  	echo "Error in updating the data"; 
 
 ?>
+
+
+<!-- Animation part -->
+<html>
+<head>
+	 <link href="assets/css/font-awesome.css" rel="stylesheet" />
+	 <link href="assets/css/loading.css" rel="stylesheet" />
+
+	</head>
+<body>
+
+
+	<div class="loader">   
+	<span class="box"></span>   
+	<span class="box"></span>  
+<div class="code"> 
+<img src="assets/img/no_background.png" width="120px">
+</div>    
+<span class="txt">Updating...</span>
+</div>
+
+	<video id="videoBG" autoplay muted loop>
+		<source src="assets/vid/Tomorrowland_scenery1.mp4" type="video/mp4">
+		</video>
+</body>
+</html>
